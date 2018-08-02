036d7c320f8eccf16d4342fd5140fa772d1f9f53
yuhanle <deadvia@gmail.com>
2018-08-02 14:33:57 +0800

    add: demo

--- Complete/wafer-php-server-sdk-master/lib/Tunnel/ITunnelHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace QCloud_WeApp_SDK\Tunnel;
-
-interface ITunnelHandler {
-    public function onRequest($tunnelId, $userInfo);
-    public function onConnect($tunnelId);
-    public function onMessage($tunnelId, $type, $content);
-    public function onClose($tunnelId);
-}
--- Compoment/wxParse-master/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    // API 接口
-    API_HOST : "http://news-at.zhihu.com/api/4/"
-}
